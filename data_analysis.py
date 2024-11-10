900800f4442c81bbb5aeae04dbb2a8bc1ad01ba9
NataliZero <ball.diamond.ru@gmail.com>
2024-11-10 17:49:08 +0300

    Add second code
    Создала новый код по поиску зарплаты по городам

--- data_analysis.py
+++ data_analysis.py
@@ -1,22 +1,11 @@
 
 import pandas as pd
 
-# Загружаем данные из файла об упражнениях
-df_exercise = pd.read_csv('Top 50 Excerice for your body.csv')
+# Загружаем данные из файла dz.csv
+df_salary = pd.read_csv('dz.csv')
 
-# Шаг 1: Вывод первых 5 строк
-print("Первые 5 строк данных о упражнениях:")
-print(df_exercise.head())
+# Расчет средней зарплаты по каждому городу
+average_salary_by_city = df_salary.groupby('City')['Salary'].mean()
 
-# Шаг 2: Информация о данных
-print("\nИнформация о данных о упражнениях:")
-print(df_exercise.info())
-
-# Шаг 3: Статистическое описание данных
-print("\nСтатистическое описание данных:")
-print(df_exercise.describe())
-
-# Шаг 4: Среднее количество калорий по уровням сложности
-avg_calories_by_difficulty = df_exercise.groupby('Difficulty Level')['Burns Calories (per 30 min)'].mean()
-print("\nСреднее количество сжигаемых калорий по уровням сложности:")
-print(avg_calories_by_difficulty)
+print("Средняя зарплата по городам:")
+print(average_salary_by_city)
